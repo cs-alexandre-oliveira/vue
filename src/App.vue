67276b535ef542e40bddd7af0bc72cc6f41cb616
Alexandre Oliveira <alexandre.oliveira@concrete.com.br>
2018-04-06 17:27:21 -0300

    implementando a home

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <img v-bind:src="foto.url" :alt="foto.titulo">
+    <form>
+      <select>
+        <option value="QA">QA</option>
+        <option value="JavaScript">JavaScript</option>
+      </select>
+      <br><br>
+      <select>
+        <option value="talks">Talks</option>
+        <option value="onboarding">Onboarding</option>
+      </select>
+      <br><br>
+      Assunto:<br>
+      <input type=""/>
+      <br><br>
+      Descrição:<br>
+      <textarea name="descricao" >
+      </textarea>
+      <br>
+      <button type="button" onclick="alert('Clickaduh!!!')">Incluir</button>
+    </form>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
+export default { 
+  data() {
+
     return {
-      //msg: 'Welcome to Your Vue.js App'
-      msg: 'Bem vindo ao seu Vue.js App'
+      titulo: 'Onboard Cadastro',
+      foto: {
+        url: 'https://thumb.lovemondays.com.br/image/096106d41e8d443e964f45fae4a30cb7/logos/93e6a6/concrete-solutions.gif',
+        titulo: 'Concrete Soluction'
+      }
+
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
