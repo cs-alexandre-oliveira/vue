8289859dd2683f2681b3bda787dc7006f0c05cb7
Alexandre DEV <calexandredev@gmail.com>
2018-04-08 07:59:30 -0300

    conectando com a API usando vue-resource

--- src/App.vue
+++ src/App.vue
@@ -35,25 +35,19 @@ export default {
 
     return {
       titulo: 'Onboard Cadastro',
-      images: [
-      {
-        url: 'https://thumb.lovemondays.com.br/image/096106d41e8d443e964f45fae4a30cb7/logos/93e6a6/concrete-solutions.gif',
-        titulo: 'Concrete Soluction'
-      },
-      {
-        url: 'https://codesui.com/app/uploads/2016/12/vuejs-logo.png',
-        titulo: 'Vue da Massa'
-      }
-      ]
+      images: []
     }
+  },
+  created() {
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+      .then(res => res.json())
+      .then(fotos => this.images = fotos, err => console.log(err));
   }
 }
 </script>
 
 <style>
-img {
-  width:100px;
-  height:50px;
-}
+
 
 </style>
